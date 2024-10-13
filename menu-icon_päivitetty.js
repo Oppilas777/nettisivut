c06eafa4917b507207db5cd2a2e31ccfc557fd47
Oppilas777 <bhl862@myy.haaga-helia.fi>
2024-10-13 14:12:56 +0300

    Delete menu-icon_päivitetty.js

--- menu-icon_päivitetty.js
+++
@@ -1,9 +0,0 @@
-// Hamburger-menu toggle
-document.getElementById('menu-icon').addEventListener('click', function () {
-    document.getElementById('nav-links').classList.toggle('show');
-});
-
-// Sulkee hamburger-menun linkin klikkauksen jälkeen
-function closeMenu() {
-    document.getElementById('nav-links').classList.remove('show');
-}
